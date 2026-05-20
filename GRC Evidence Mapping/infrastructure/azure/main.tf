a4d7d9ffba66702b9103acfcc647eda516f80dc4
Nathaniel Konu <nateinfowave@gmail.com>
2026-05-19 20:18:06 -0600

    Add Azure Storage Account resource configuration

--- GRC Evidence Mapping/infrastructure/azure/main.tf
+++ GRC Evidence Mapping/infrastructure/azure/main.tf
@@ -1,1 +1,7 @@
-
+resource "azurerm_storage_account" "secure_data" {
+  name                     = "storageeachub2026"
+  resource_group_name      = "compliance-rg"
+  location                 = "West US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
